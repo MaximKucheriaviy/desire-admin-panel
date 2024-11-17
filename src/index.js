891f79668d438ccc7bdbd35dd3f860d36c97f0b2
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2024-11-17 09:42:09 +0200

    for netlify fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { store } from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <App />
